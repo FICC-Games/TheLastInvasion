f4db77448d0875dd341fcb7dfdbe048775ecc487
BananaSplit6785 <efe.erdal@elev.ga.lbs.se>
2022-01-21 13:40:40 +0100

    my mom

---
+++ Assets/enemy raycast.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyraycast : MonoBehaviour
+{
+
+    Vector3 directionToPlayer;
+    Transform playerTransform;
+    [SerializeField]
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = FindObjectOfType<player>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        directionToPlayer = (playerTransform.position - transform.position).normalized;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, directionToPlayer, 5);
+        if (hit.transform != null)
+        {
+
+            if (hit.transform.tag == "player")
+            {
+                transform.position += directionToPlayer * 1 * Time.deltaTime;
+            }
+
+
+        }
+
+
+    }
+   
+}
