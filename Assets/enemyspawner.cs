4c7dac70b0347845779d8455c5e4bb2d109c1b45
BananaSplit6785 <efe.erdal@elev.ga.lbs.se>
2022-02-23 09:39:46 +0100

    f

--- Assets/enemyspawner.cs
+++ Assets/enemyspawner.cs
@@ -7,7 +7,6 @@ public class enemyspawner : MonoBehaviour
     [SerializeField]
     private GameObject enemy;
     private GameObject newenemy;
-    private SpriteRenderer rend;
     private int randomSpawnZone;
     private float randomXposition, randomYposition;
     private Vector3 spawnposition;
@@ -16,7 +15,7 @@ public class enemyspawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("spawnenemy", 0f, 10f);
+        InvokeRepeating("spawnenemy", 100f, 100f);
     }
 
 
@@ -28,19 +27,19 @@ public class enemyspawner : MonoBehaviour
         {
             case 0:
                 randomXposition = Random.Range(-3f, 8f);
-                randomYposition = Random.Range(-2f, 4f);
+                randomYposition = Random.Range(0f, 0f);
                 break;
             case 1:
                 randomXposition = Random.Range(-5f, 1f);
-                randomYposition = Random.Range(-4f, 6f);
+                randomYposition = Random.Range(0f, 0f);
                 break;
             case 2:
                 randomXposition = Random.Range(-2f, 0f);
-                randomYposition = Random.Range(-3f, 7f);
+                randomYposition = Random.Range(-0f, 0f);
                 break;
             case 3:
                 randomXposition = Random.Range(-3f, 5f);
-                randomYposition = Random.Range(-2f, 10f);
+                randomYposition = Random.Range(0f, 0f);
                 break;
 
 
@@ -49,8 +48,9 @@ public class enemyspawner : MonoBehaviour
 
 
         }
-        
-      
+        spawnposition = new Vector3(randomXposition, randomYposition, 0f);
+        newenemy = Instantiate(enemy, spawnposition, Quaternion.identity);
+
     }
     
 }
